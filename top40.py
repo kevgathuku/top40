fbae1592e9a94a4b6faec0896abfc38acb06d5d2
Kevin Ndung'u <kevgathuku@gmail.com>
2014-11-27 10:56:19 +0300

    Implement basic printing of the singles chart

--- top40.py
+++ top40.py
@@ -1,8 +1,27 @@
+#/usr/bin/env python
+# -*- coding: utf-8 -*-
+
 import click
 import requests
 
 url = 'http://ben-major.co.uk/labs/top40/api/singles/'
 
-response = requests.get(url)
+@click.command()
+@click.option('--count',
+    default=10,
+    help='Number of songs to show. Maximum is 40')
 
-print response.json()
+def get_charts(count):
+    """Prints the top COUNT songs in the UK Top 40 chart."""
+
+    response = requests.get(url).json()
+    data = response['entries'][:count]
+    for index, element in enumerate(data, start=1):
+        click.echo(
+            '{}. {} - {}'.format(
+                index,
+                element['title'],
+                element['artist'].encode('utf-8', 'replace')))
+
+if __name__ == '__main__':
+    get_charts()
