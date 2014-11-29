d8160022501033e2b40b6c888ab22c4e0c3c75b3
Kevin Ndung'u <kevgathuku@gmail.com>
2014-11-29 16:50:12 +0300

    Implement basic downloading with hardcoded song position

--- top40.py
+++ top40.py
@@ -4,6 +4,7 @@ import os
 import click
 import requests
 import requests_cache
+import youtube_dl
 
 from apiclient.discovery import build
 from apiclient.errors import HttpError
@@ -24,11 +25,11 @@ def get_charts():
 
     return data
 
-@click.command()
-@click.option('-c', '--count',
-    type=click.IntRange(1, 40, clamp=True),
-    default=10,
-    help='Number of songs to show. Maximum is 40')
+# @click.command()
+# @click.option('-c', '--count',
+#     type=click.IntRange(1, 40, clamp=True),
+#     default=10,
+#     help='Number of songs to show. Maximum is 40')
 
 def print_charts(count):
     """Prints the top COUNT songs in the UK Top 40 chart."""
@@ -42,12 +43,12 @@ def print_charts(count):
                 element['title'],
                 element['artist'].encode('utf-8', 'replace')))
 
-@click.command()
-@click.option('-q', '--query',
-    type=click.STRING,
-    help='Search query')
+# @click.command()
+# @click.option('-q', '--query',
+#     type=click.STRING,
+#     help='Search query')
 
-def youtube_search(query, max_results=5):
+def youtube_search(query, max_results=1):
     """Search Youtube for QUERY, returning MAX_RESULTS
         Returns a dict of video ID and TITLE mappings
     """
@@ -66,15 +67,33 @@ def youtube_search(query, max_results=5):
     # Add each result to a dict, and then display the lists of
     # matching videos
 
-    videos = {search_result["id"]["videoId"]: search_result["snippet"]["title"]
+    videos = {search_result["snippet"]["title"]: search_result["id"]["videoId"]
     for search_result in search_response.get("items", []) 
     if search_result["id"]["kind"] == "youtube#video"}
 
-    print "Videos:\n"
-    for key, val in videos.items():
-            print "ID: {} Title: {}".format(key, val), "\n"
+    # print "Videos:\n"
+    # for key, val in videos.items():
+    #         print "ID: {} Title: {}".format(key, val), "\n"
 
+    # Dict in format {title: id}
     return videos
 
+def download_songs(pos):
+    """Downloads the song occupying the POS spot in the charts"""
+
+    data = get_charts()
+    pos -=1
+    
+    search = '{} - {}'.format(
+                data[pos]['title'],
+                data[pos]['artist'].encode('utf-8', 'replace'))
+    dl = youtube_search(search)
+
+    ydl_opts = {}
+    with youtube_dl.YoutubeDL(ydl_opts) as ydl:
+        print "Downloading " + dl.keys()[0]
+        ydl.download(dl.values())
+
+
 if __name__ == '__main__':
-    youtube_search()
+    download_songs(1)
