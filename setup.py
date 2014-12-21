80185963a5ea507b1010ad24dc9db2fbc34e587b
Kevin Ndung'u <kevgathuku@gmail.com>
2014-12-21 23:14:46 +0300

    Update version to 0.2.2 Remove youtube-dl dependency. Update description

--- setup.py
+++ setup.py
@@ -5,11 +5,11 @@ from setuptools import setup
 
 setup(
     name='top40',
-    version='0.1.2',
+    version='0.2.0',
     py_modules=['top40'],
     author = "Kevin Ndung'u",
     author_email = 'kevgathuku@gmail.com',
-    description = ("Display and optionally download songs in the "
+    description = ("A simple command line tool to display songs in the "
                     "UK Top 40 Charts"),
     url='https://github.com/kevgathuku/top40',
     license = "MIT",
@@ -18,7 +18,6 @@ setup(
         'requests>=2.4.3',
         'requests-cache==0.4.6',
         'google-api-python-client==1.3.1',
-        'youtube-dl>=2014.12.01',
     ],
     entry_points='''
         [console_scripts]
